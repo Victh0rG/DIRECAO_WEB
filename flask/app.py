f1800067a678423d20dc1b45647989a0b2c92e9f
unknown <victhorgb1080@gmail.com>
2024-04-14 16:51:29 -0300

    atualizando documentação

--- flask/app.py
+++ flask/app.py
@@ -2,25 +2,44 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# rota principal
+# Rota principal
 @app.route('/')
 def index():
-    return render_template('login.html')  # 'templates'
+    """Renderiza a página de login.
 
-#rota para login
+    Returns:
+        str: O conteúdo HTML da página de login.
+    """
+    return render_template('login.html')
+
+# Rota para efetuar o login
 @app.route('/login', methods=['POST'])
 def login():
+    """Processa a tentativa de login.
+
+    Obtém as credenciais do formulário de login.
+    Verifica as credenciais e redireciona para a página de sucesso se forem corretas.
+
+    Returns:
+        str: Redireciona para a página de sucesso ou exibe uma mensagem de falha no login.
+    """
     username = request.form['username']
     password = request.form['password']
     
-    # credenciais
+    # Verifica as credenciais
     if username == "admin@example.com" and password == "secret":
         return redirect(url_for('success'))
     else:
         return "Falha no login!"
 
+# Rota para a página de sucesso
 @app.route('/success')
 def success():
+    """Página de sucesso após o login.
+
+    Returns:
+        str: O conteúdo HTML da página de sucesso.
+    """
     return "Login bem-sucedido!"
 
 if __name__ == '__main__':
